015dda556407abd8d126374571fc4cff2b1ae763
Vernon, Chris R <chris.vernon@pnnl.gov>
2017-09-07 13:40:31 -0400

    updating package retrieval method

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+
 setup(
     name='demeter',
     version='1.0.0',
-    packages=['demeter.change', 'demeter.weight', 'demeter', 'demeter.demeter_io'],
+    packages=find_packages(),
     url='https://github.com/IMMM-SFA/demeter',
     license='BSD 2-Clause',
     author='Chris R. Vernon; Yannick le Page',
     author_email='chris.vernon@pnnl.gov; niquya@gmail.com',
     description='A land use land cover disaggregation and land change analytic model',
     long_description=readme(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'fiona',
-        'netCDF4',
-        'pandas'
-    ]
+    install_requires=get_requirements()
 )
