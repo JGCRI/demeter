73038b0388712652fb27531ae58c7c561920890c
crvernon <chris.vernon@pnnl.gov>
2020-02-27 16:11:24 -0500

    raise Demeter class

--- demeter/__init__.py
+++ demeter/__init__.py
@@ -9,5 +9,10 @@ Open source under license BSD 2-Clause - see LICENSE and DISCLAIMER
 """
 
 
+from demeter.model import Demeter
+
+
 __author__ = "Chris R. Vernon (chris.vernon@pnnl.gov); Yannick le Page (niquya@gmail.com)"
 __version__ = '1.0.0'
+
+__all__ = ['Demeter']
