45aa287a9c37a58a82aa274bcbe3f6d6569a63a9
crvernon <chrisrvernon@gmail.com>
2024-05-29 17:58:39 -0400

    update imports

--- demeter/__init__.py
+++ demeter/__init__.py
@@ -11,5 +11,5 @@ from .model import Model, run_model
 from .install_supplement import get_package_data
 
 from .preprocess_data import format_gcam_data, FormatGcamDataFrame
-from .post_process.demeter_plotter import LandCoverPlotter as LandCoverPlotter
-from .post_process.post_process_results import DataProcessor as DataProcessor
+from .post_process.demeter_plotter import LandCoverPlotter
+from .post_process.post_process_results import DataProcessor
