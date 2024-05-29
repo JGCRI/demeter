59443a2764c06ab22e6d6a4a2704ed0fdbef24b2
crvernon <chrisrvernon@gmail.com>
2024-05-29 17:01:48 -0400

    fix nested referencing

--- demeter/__init__.py
+++ demeter/__init__.py
@@ -7,9 +7,9 @@ Open source under license BSD 2-Clause - see LICENSE and DISCLAIMER
 
 @author:  Chris R. Vernon (PNNL); Yannick le Page (niquya@gmail.com)
 """
-from demeter.model import Model, run_model
-from demeter.install_supplement import get_package_data
+from .model import Model, run_model
+from .install_supplement import get_package_data
 
-from demeter.preprocess_data import format_gcam_data, FormatGcamDataFrame
-from demeter.post_process.demeter_plotter import LandCoverPlotter
-from demeter.post_process.post_process_results import DataProcessor
+from .preprocess_data import format_gcam_data, FormatGcamDataFrame
+from .post_process.demeter_plotter import LandCoverPlotter as LandCoverPlotter
+from .post_process.post_process_results import DataProcessor as DataProcessor
